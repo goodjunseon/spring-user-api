eede6b3387c4a3459b1a5046fcd57d14cc07c438
goodjunseon <pzs2001926@gmail.com>
2025-05-18 12:51:15 +0900

    feat: AccessToken 만료시, RefreshToken 검증하여 재발급 로직 작성

--- user-api/src/main/java/com/goodjunseon/user_api/domain/member/controller/AuthController.java
+++ user-api/src/main/java/com/goodjunseon/user_api/domain/member/controller/AuthController.java
@@ -1,6 +1,12 @@
 package com.goodjunseon.user_api.domain.member.controller;
 
+import com.goodjunseon.user_api.global.security.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +14,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-//    private final JWTUtil jwtUtil;
-//    private final JWTService jwtService;
+
+    private final JWTUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("token/refresh")
+    public ResponseEntity<?> refreshAccessToken(HttpServletRequest request) {
+        String refreshToken = request.getHeader("Authorization").replace("Bearer ", "");
+
+        // Refresh Token 검증
+        if (!jwtUtil.validateToken(refreshToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+        }
+
+        String email = jwtUtil.getUsername(refreshToken);
+        String storedRefreshToken = redisTemplate.opsForValue().get("refreshToken:" + email);
+
+        if (!refreshToken.equals(storedRefreshToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+        }
+
+        String newAccessToken = jwtUtil.createToken(null, email, jwtUtil.getRole(refreshToken)); // idx는 생략가능
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + newAccessToken)
+                .body("Access token refreshed successfully");
+
+    }
 
 
 
