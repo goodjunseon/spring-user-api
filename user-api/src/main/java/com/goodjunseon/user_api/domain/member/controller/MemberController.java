ee9810a0f05382e0c10cc9f66abb566d8b4833cf
goodjunseon <pzs2001926@gmail.com>
2025-05-18 14:37:40 +0900

    feat: JoinController + UserController -> MemberController로 병합

--- user-api/src/main/java/com/goodjunseon/user_api/domain/member/controller/MemberController.java
+++ user-api/src/main/java/com/goodjunseon/user_api/domain/member/controller/MemberController.java
@@ -1,20 +1,19 @@
 package com.goodjunseon.user_api.domain.member.controller;
 
 import com.goodjunseon.user_api.domain.member.model.entity.Member;
+import com.goodjunseon.user_api.domain.member.model.request.MemberJoinReq;
 import com.goodjunseon.user_api.domain.member.model.response.MemberRes;
 import com.goodjunseon.user_api.domain.member.service.MemberService;
 import com.goodjunseon.user_api.global.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
-public class UserController {
+public class MemberController {
 
     private final MemberService memberService;
 
@@ -22,10 +21,19 @@ public class UserController {
      * User (CRUD) API 구현
      */
 
-    // 전체 사용자 목록 조회
+    // 회원가입 로직
+    @PostMapping("/signup")
+    public String join(@RequestBody MemberJoinReq req) {
+        // 회원가입 처리 로직
+        memberService.signup(req);
 
-    @GetMapping("/")
-    public BaseResponse<List<MemberRes>> getAllUsers() {
+        // 아직까지는 프론트에 Response를 보내는 부분이 없으므로
+        return "회원가입 성공";
+    }
+
+    // 전체 사용자 목록 조회
+    @GetMapping("/getAll")
+    public BaseResponse<List<MemberRes>> getAllMembers() {
         // 사용자 목록 조회 로직
         // 예시로 "사용자 목록" 문자열 반환
         List<Member> members = memberService.findAll();
