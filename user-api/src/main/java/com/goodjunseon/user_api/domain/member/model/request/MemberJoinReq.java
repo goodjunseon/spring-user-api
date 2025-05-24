4c68b43dd50ef7fc180c1cebcfd9a9999f2c7627
goodjunseon <pzs2001926@gmail.com>
2025-05-25 08:22:42 +0900

    Feat: Valid 추가

--- user-api/src/main/java/com/goodjunseon/user_api/domain/member/model/request/MemberJoinReq.java
+++ user-api/src/main/java/com/goodjunseon/user_api/domain/member/model/request/MemberJoinReq.java
@@ -1,5 +1,8 @@
 package com.goodjunseon.user_api.domain.member.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +10,12 @@ import lombok.Setter;
 @Getter
 public class MemberJoinReq {
 
-
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String password;
     private String nickname;
 
